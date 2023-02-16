2e9860bd7d2578d9a1eb64a7e8713e594400f1a0
Elivelton Ferreira <elivelton.seven@gmail.com>
2023-02-16 13:34:06 -0300

    :sparkles: add itemList component

---
+++ src/components/ItemList.vue
@@ -0,0 +1,64 @@
+<template lang="">
+  <div class="items-list">
+    <Loading v-show="isLoading" />
+    <Item v-for="item in itemsList" :key="item.id" :item="item" />
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+import Item from "./ItemProduct.vue";
+import Loading from "./LoadingCategory.vue";
+
+export default {
+  name: "ItemsList",
+  components: {
+    Item,
+    Loading,
+  },
+  data() {
+    return {
+      itemsList: [],
+      isLoading: false,
+    };
+  },
+  created() {},
+  computed: {
+    selectedCategory: {
+      get() {
+        return this.$store.state.selectedCategory;
+      },
+    },
+  },
+  methods: {
+    getItemsList() {
+      this.isLoading = true
+      this.itemsList = []
+      
+      setTimeout(() => {
+        axios
+          .get(`http://localhost:3000/${this.selectedCategory}`)
+          .then((response) => {this.itemsList = response.data; this.isLoading=false});
+      }, 2000);
+    },
+  },
+  watch: {
+    selectedCategory() {
+      this.getItemsList();
+    },
+  },
+};
+</script>
+
+<style lang="less" scoped>
+.items-list {
+  margin: 50px;
+  display: flex;
+  flex-wrap: wrap;
+  width: 100%;
+
+  @media @tablets {
+    margin: 20px;
+  }
+}
+</style>
