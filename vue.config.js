22a721877758e3f8fc00857065a3af0e56a1f932
Elivelton Ferreira <elivelton.seven@gmail.com>
2023-02-16 13:31:01 -0300

    :sparkles: configuring less and svg loader

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,24 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  css: {
+    loaderOptions: {
+      less:{
+        additionalData: `@import "~@/styles/variables.less";`
+      }
+    }
+  },
+  chainWebpack: (config) => {
+
+    const svgRule = config.module.rule('svg');
+ 
+    svgRule.uses.clear();
+ 
+    svgRule
+      .use('babel-loader')
+      .loader('babel-loader')
+      .end()
+      .use('file-loader')
+      .loader('vue-svg-loader');
+  },
 })
