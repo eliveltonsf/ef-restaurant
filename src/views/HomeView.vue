ee2b7699313af8f498a7d17c532f0cb03c256c99
Elivelton Ferreira <elivelton.seven@gmail.com>
2023-02-16 13:47:28 -0300

    :sparkles: configuring router view and store

---
+++ src/views/HomeView.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="home">
+    <CategoryMenu />
+    <ItemList />
+  </div>
+</template>
+
+<script>
+import CategoryMenu from "@/components/CategoryMenu.vue";
+import ItemList from "@/components/ItemList.vue";
+
+export default {
+  name: "HomeView",
+  components: {
+    CategoryMenu,
+    ItemList,
+  },
+};
+</script>
+
+<style lang="less">
+.home {
+  display: flex;
+
+  @media @tablets {
+    flex-direction: column;
+  }
+}
+</style>
