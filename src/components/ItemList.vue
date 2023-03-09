9d3822d7ae8cc44bf33f67d67988e6409a63b038
Elivelton Ferreira <elivelton.seven@gmail.com>
2023-03-09 11:56:52 -0300

    :sparkles: use of loading and product listing

--- src/components/ItemList.vue
+++ src/components/ItemList.vue
@@ -1,5 +1,5 @@
 <template lang="">
-  <div class="items-list">
+  <div :class="!isLoading ? 'items-list' : 'loadingContainer'">
     <Loading v-show="isLoading" />
     <Item v-for="item in itemsList" :key="item.id" :item="item" />
   </div>
@@ -22,24 +22,25 @@ export default {
       isLoading: false,
     };
   },
-  created() {},
+  created() {
+    this.getItemsList();
+  },
   computed: {
-    selectedCategory: {
-      get() {
-        return this.$store.state.selectedCategory;
-      },
+    selectedCategory() {
+      return this.$store.state.selectedCategory;
     },
   },
   methods: {
     getItemsList() {
       this.isLoading = true
+      this.$store.dispatch("changeStateLoading", true);
       this.itemsList = []
       
       setTimeout(() => {
         axios
           .get(`http://localhost:3000/${this.selectedCategory}`)
-          .then((response) => {this.itemsList = response.data; this.isLoading=false});
-      }, 2000);
+          .then((response) => {this.itemsList = response.data; this.isLoading=false;  this.$store.dispatch("changeStateLoading", false);});
+      }, 1000);
     },
   },
   watch: {
@@ -51,14 +52,29 @@ export default {
 </script>
 
 <style lang="less" scoped>
+
+.loadingContainer{
+  margin: 50px;
+  display: flex;
+  flex-wrap: nowrap;
+  width: 100%;
+  align-content: flex-start;
+
+  @media @tablets {
+    margin: 20px;
+    width: auto;  
+  }
+}
 .items-list {
   margin: 50px;
   display: flex;
   flex-wrap: wrap;
   width: 100%;
+  align-content: flex-start;
 
   @media @tablets {
     margin: 20px;
+    width: auto;
   }
 }
 </style>
