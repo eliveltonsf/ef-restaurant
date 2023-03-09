bb7dfdb5768add4af40c56429ce26da9d87ffb66
Elivelton Ferreira <elivelton.seven@gmail.com>
2023-03-09 11:49:07 -0300

    :sparkles: quantity control of the products inside the cart

--- src/store/store.js
+++ src/store/store.js
@@ -5,16 +5,71 @@ Vue.use(Vuex);
 
 export const store = new Vuex.Store({
   state: {
-    selectedCategory: '',
+    selectedCategory: "",
+    cartList: [],
+    isLoading: false,
   },
   mutations: {
     changeCategory(state, id) {
-      state.selectedCategory = id
+      state.selectedCategory = id;
+    },
+    changeStateLoading(state, value) {
+      state.isLoading = value;
+    },
+    addToCart(state, product) {
+      state.cartList.push({ ...product, quantity: product.quantity || 1 });
+    },
+    increaseQuantity(state, {index, quantity}) {
+      state.cartList[index].quantity += quantity;
+    },
+    decreaseQuantity(state, index) {
+      const quantity = state.cartList[index].quantity;
+      if (quantity === 0) {
+        state.cartList[index].quantity = 0;
+      } else {
+        --state.cartList[index].quantity;
+      }
     },
   },
   actions: {
     changeCategory(context, id) {
       context.commit("changeCategory", id);
+    },
+    changeStateLoading(context, value) {
+      context.commit("changeStateLoading", value);
+    },
+    addToCart({ state, commit }, product) {
+      const isProductList = state.cartList.find(
+        (item) => item.id === product.id
+      );
+      if (!isProductList) {
+        commit("addToCart", product);
+        return;
+      }
+
+      const isProductIndex = state.cartList.findIndex(
+        (item) => item.id === product.id
+      );
+      commit("increaseQuantity", {
+        index: isProductIndex,
+        quantity: product?.quantity || 1,
+      });
+
+    },
+    increaseQuantity({ state, commit }, id) {
+      const isProductIndex = state.cartList.findIndex((item) => item.id === id);
+      commit("increaseQuantity", {index: isProductIndex, quantity: 1});
+    },
+    decreaseQuantity({ state, commit }, id) {
+      const isProductIndex = state.cartList.findIndex((item) => item.id === id);
+      commit("decreaseQuantity", isProductIndex);
+    },
+  },
+  getters: {
+    getCartTotal: (state) => {
+      return state.cartList.reduce((acc, item) => {
+        return acc + item.price * item.quantity;
+      }, 0);
     },
   },
 });
