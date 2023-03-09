89cc26f50462563d89959d35ff25884a861149fc
Elivelton Ferreira <elivelton.seven@gmail.com>
2023-03-09 11:47:41 -0300

    :sparkles: add the other routes of the project

--- src/router/index.js
+++ src/router/index.js
@@ -1,18 +1,31 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import ShoppingCart from '@/components/ShoppingCart.vue'
+import AddToCart from '@/components/AddToCart.vue';
 
 Vue.use(VueRouter)
 
 const routes = [
   {
     path: '/',
-    name: 'home',
+    name: 'Home',
     component: HomeView
   },
   {
+    path: '/cart',
+    name: 'ShoppingCart',
+    component: ShoppingCart
+  },
+  {
+    path: '/addToCart/:id',
+    name: 'AddToCart',
+    component: AddToCart,
+    props: true
+  },
+  {
     path: '/about',
-    name: 'about',
+    name: 'About',
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
